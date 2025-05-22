f8c0f8f4e7af4a6dd0396cbad9e9a2a52443223b
Bhuvaneshwari-12 <bhuvaneshwaribaskar12@gmail.com>
2025-05-22 15:32:00 +0530

    Create DataTypeDemo.java

---
+++ Corejava/DataTypeDemo.java
@@ -0,0 +1,15 @@
+public class DataTypeDemo {
+    public static void main(String[] args) {
+        int number = 25;
+        float floatNumber = 3.14f;
+        double doubleNumber = 3.1415926535;
+        char letter = 'A';
+        boolean isJavaFun = true;
+
+        System.out.println("Integer value: " + number);
+        System.out.println("Float value: " + floatNumber);
+        System.out.println("Double value: " + doubleNumber);
+        System.out.println("Character value: " + letter);
+        System.out.println("Boolean value: " + isJavaFun);
+    }
+}
